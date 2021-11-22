8c3b9bfb3a6676aa18c98f2a7188ede2302fb79f
Roel Stappers <roel.stappers@met.no>
2021-11-22 17:15:35 +0100

    Add docs

---
+++ docs/make.jl
@@ -0,0 +1,16 @@
+using Documenter
+
+
+pages = filter!(x -> x!="index.md", readdir("src/")) 
+
+makedocs(
+    sitename = "Playground wiki",
+    format = Documenter.HTML(prettyurls = true), 
+    pages = ["index.md", pages...]
+)
+
+deploydocs(
+   repo = "github.com/Hirlam/PlayGround.git",
+   devbranch = "develop", 
+   devurl = "dev",
+)

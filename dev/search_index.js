406b8021bf3f4d5f9713a11b6658e2fc671372cb
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-11-30 11:03:04 +0000

    build based on 92aa753

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"roelstappers/#Roel-Stappers-1","page":"Roel Stappers","title":"Roel Stappers","text":"","category":"section"},{"location":"#Home-1","page":"Home","title":"Home","text":"","category":"section"}]
+[{"location":"roelstappers/#Roel-Stappers-1","page":"Roel Stappers","title":"Roel Stappers","text":"","category":"section"},{"location":"roelstappers/#","page":"Roel Stappers","title":"Roel Stappers","text":"hallo","category":"page"},{"location":"#Home-1","page":"Home","title":"Home","text":"","category":"section"}]
 }

c87223761bf6d809af0a68acfe3847b7499fdd2f
Roel Stappers <roels@met.no>
2022-02-28 23:57:19 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ pages = filter!(x -> x!="index.md", readdir("docs/src/"))
 format= Documenter.HTML(prettyurls = true, 
                         edit_link="develop",
                         analytics="UA-221448594-2", 
-                        footer="test"
+                        footer="<a href=\"javascript:__gaTrackerOptout()\">Click here to opt-out of Google Analytics</a>"
                    )
 
 makedocs(
